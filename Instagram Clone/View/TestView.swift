0643fd985f7cb2b3fbf4a85f6e6eaa7b67d92387
Burak Cüce <b.cuece@web.de>
2022-06-12 14:17:07 +0200

    - add TestView

---
+++ Instagram Clone/View/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  Instagram Clone
+//
+//  Created by Burak Cüce on 12.06.22.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
