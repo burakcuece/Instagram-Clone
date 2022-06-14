e50a7b06f5d8647414ddf201fb23938503ec8879
Burak Cüce <b.cuece@web.de>
2022-06-14 20:52:18 +0200

    - edit RecoverPasswordWithEmailView and RecoverPasswordWithPhoneView

--- Instagram Clone/View/RecoverPasswordWithEmailView.swift
+++ Instagram Clone/View/RecoverPasswordWithEmailView.swift
@@ -18,68 +18,71 @@ struct RecoverPasswordWithEmailView: View {
         
         
         
-        VStack {
+        NavigationView {
             
-            
-            TextField("Benutzername oder E-Mail Adresse", text: $email)
-                .keyboardType(.emailAddress)
-                .padding()
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(0.5)
-                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
-                .shadow(color: Color.black.opacity(0.08), radius: 5, x: 0, y: -5)
-            
-            Button {
-                print("Weiter")
-            } label: {
-                Text("Weiter")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 40)
-                    .background(Color.blue.opacity(0.8))
-                    .cornerRadius(8)
-            }
-            .padding()
-            
-            Button {
-                print("Benötigst du weitere Hilfe?")
-            } label: {
-                Text("Benötigst du weitere Hilfe?")
-            }
-            .padding()
-            
-            Text("ODER")
-                .foregroundColor(Color.gray)
-                .padding()
-            
-            
-            Button {
-                print("Mit Facebook anmelden")
-            } label: {
+            VStack {
                 
-                HStack {
-                    Image("Facebook-logo")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                    Text("Mit Facebook anmelden")
-                        .bold()
+                
+                TextField("Benutzername oder E-Mail Adresse", text: $email)
+                    .keyboardType(.emailAddress)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(0.5)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+                    .shadow(color: Color.black.opacity(0.08), radius: 5, x: 0, y: -5)
+                
+                Button {
+                    print("Weiter")
+                } label: {
+                    Text("Weiter")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 25)
+                        .background(Color.blue.opacity(0.8))
+                        .cornerRadius(8)
                 }
-                .padding(.bottom, 150)
-            }
-            
-            Button {
-                self.isPresented.toggle()
+                .padding()
                 
-            } label: {
-                Text("Zurück zur Anmeldung")
+                Button {
+                    print("Benötigst du weitere Hilfe?")
+                } label: {
+                    Text("Benötigst du weitere Hilfe?")
+                }
+                .padding()
+                
+                Text("ODER")
+                    .foregroundColor(Color.gray)
+                
+                
+                Button {
+                    print("Mit Facebook anmelden")
+                } label: {
+                    
+                    HStack {
+                        Image("Facebook-logo")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                        Text("Mit Facebook anmelden")
+                            .bold()
+                        
+                    }
+                    .padding(.bottom, 100)
+                }
+                
+                Button {
+                    self.isPresented.toggle()
+                    
+                } label: {
+                    Text("Zurück zur Anmeldung")
+                }
+                .fullScreenCover(isPresented: $isPresented, content: {
+                    LoginView()
+                })
             }
-            .fullScreenCover(isPresented: $isPresented, content: {
-                LoginView()
-            })
+            .padding()
         }
-        .padding()
     }
     
 }
--- Instagram Clone/View/RecoverPasswordWithPhoneView.swift
+++ Instagram Clone/View/RecoverPasswordWithPhoneView.swift
@@ -18,59 +18,59 @@ struct RecoverPasswordWithPhoneView: View {
     var body: some View {
         
         
-        VStack {
-            
-            
-            NumberField()
-            
-            Button {
-                print("Weiter")
-            } label: {
-                Text("Weiter")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 40)
-                    .background(Color.blue.opacity(0.8))
-                    .cornerRadius(8)
+        NavigationView {
+            VStack {
                 
-            }
-            .padding()
-            
-            Button {
-                print("Benötigst du weitere Hilfe?")
-            } label: {
-                Text("Benötigst du weitere Hilfe?")
-            }
-            .padding()
-            
-            Text("ODER")
-                .foregroundColor(Color.gray)
-                .padding()
-            
-            Button {
-                print("Mit Facebook anmelden")
-            } label: {
-                HStack {
-                    Image("Facebook-logo")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                    Text("Mit Facebook anmelden")
-                        .bold()
+                
+                NumberField()
+                
+                Button {
+                    print("Weiter")
+                } label: {
+                    Text("Weiter")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 20)
+                        .background(Color.blue.opacity(0.8))
+                        .cornerRadius(8)
+                    
                 }
-                .padding(.bottom, 100)
-            }
+                
+                Button {
+                    print("Benötigst du weitere Hilfe?")
+                } label: {
+                    Text("Benötigst du weitere Hilfe?")
+                }
+                .padding()
+                
+                Text("ODER")
+                    .foregroundColor(Color.gray)
+                
+                Button {
+                    print("Mit Facebook anmelden")
+                } label: {
+                    HStack {
+                        Image("Facebook-logo")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                        Text("Mit Facebook anmelden")
+                            .bold()
+                    }
+                    .padding(.bottom, 100)
+                }
 
-            Button {
-                self.isPresented.toggle()
-            } label: {
-                Text("Zurück zur Anmeldung")
+                Button {
+                    self.isPresented.toggle()
+                } label: {
+                    Text("Zurück zur Anmeldung")
+                }
             }
-        }
-        .fullScreenCover(isPresented: $isPresented, content: {
-            LoginView()
+            .fullScreenCover(isPresented: $isPresented, content: {
+                LoginView()
         })
+        }
     }
 }
 
