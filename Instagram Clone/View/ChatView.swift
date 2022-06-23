48e887e4d4c5efaea2e7e0a6b94275e33a008e42
Burak Cüce <b.cuece@web.de>
2022-06-23 16:51:00 +0200

    - edit ChatView

--- Instagram Clone/View/ChatView.swift
+++ Instagram Clone/View/ChatView.swift
@@ -8,15 +8,59 @@
 import SwiftUI
 
 struct ChatView: View {
+    @StateObject var viewModel = ChatViewModel()
+    @State private var query = ""
+    
     var body: some View {
         NavigationView {
-            Text("Hello, World!")
+            
+            List {
+                
+                ForEach(viewModel.getSortedFilteredChats(query: query) ) { chat in
+                    
+                    ZStack {
+                        
+                        ChatRowView(chat: chat)
+                        
+                        NavigationLink {
+                            ChatInView(chat: chat)
+                                .environmentObject(viewModel)
+                        } label: {
+                            EmptyView()
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .frame(width: 0)
+                        .opacity(0)
+                    }
+                    .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                        Button(action: {
+                            viewModel.marksAsUnread(!chat.hasUnreadMessage, chat: chat)
+                        }) {
+                            if chat.hasUnreadMessage {
+                                Label("Gelesen", systemImage: "text.bubble")
+                            } else {
+                                Label("Ungelesen", systemImage: "circle.fill")
+                            }
+                        }
+                        .tint(.blue)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .searchable(text: $query)
+            .navigationTitle("Chats")
+            .navigationBarItems(trailing: Button(action: {}, label: {
+                Image(systemName: "square.and.pencil")
+            }))
         }
+        
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
         ChatView()
+        
     }
 }
+
