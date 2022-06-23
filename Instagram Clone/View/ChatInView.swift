7a5c805161445c387546a3851f614efe3e22ed07
Burak Cüce <b.cuece@web.de>
2022-06-23 16:47:27 +0200

    - edit ChatRowView and ChatInView

--- Instagram Clone/View/ChatInView.swift
+++ Instagram Clone/View/ChatInView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ChatInView: View {
     
-    @EnvironmentObject var viewMoel : ChatViewModel
+    @EnvironmentObject var viewModel : ChatViewModel
     
     let chat: Chat
     
@@ -19,13 +19,151 @@ struct ChatInView: View {
     @State private var messageIDToScroll: UUID?
     
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            GeometryReader { reader in
+                ScrollView {
+                    ScrollViewReader { scrollReader in
+                        getMessagesView(viewWidth: reader.size.width)
+                            .padding(.horizontal)
+                            .onChange(of: messageIDToScroll) { _ in
+                                if let messageID = messageIDToScroll {
+                                    scrollTo(messageID: messageID, shouldAnimate: true, scrollReader: scrollReader)
+                                }
+                            }
+                            .onAppear {
+                                if let messageID = chat.messages.last?.id {
+                                    scrollTo(messageID: messageID, anchor: .bottom, shouldAnimate: true, scrollReader: scrollReader)
+                                }
+                            }
+                    }
+                }
+            }
+            .padding(.bottom, 5)
+            
+            toolbarView()
+        }
+        .padding(.top, 1)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(leading: navBarLeadingBtn)
+        .onAppear {
+            viewModel.marksAsUnread(false, chat: chat)
+        }
     }
+    
+    var navBarLeadingBtn: some View {
+        Button(action: {
+            
+        }) {
+            HStack {
+                Image(chat.person.imgString)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text(chat.person.name).bold()
+            }
+            .foregroundColor(.black)
+        }
+    }
+    
+    func scrollTo(messageID: UUID, anchor: UnitPoint? = nil, shouldAnimate: Bool, scrollReader: ScrollViewProxy) {
+        DispatchQueue.main.async {
+            withAnimation(shouldAnimate ? Animation.easeIn : nil) {
+                scrollReader.scrollTo(messageID, anchor: anchor)
+            }
+        }
+    }
+    
+    func toolbarView() -> some View {
+        VStack {
+            let height: CGFloat = 38
+            HStack {
+                TextField("Nachricht", text: $text)
+                    .padding(.horizontal, 10)
+                    .frame(height: height)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 13))
+                    .focused($isFocused)
+                
+                Button(action: sendMessage) {
+                    Image(systemName: "paperplane.fill")
+                        .foregroundColor(.white)
+                        .frame(width: height, height: height)
+                        .background(
+                            Circle()
+                                .foregroundColor(text.isEmpty ? .gray : .blue)
+                        )
+                }
+                .disabled(text.isEmpty)
+            }
+            .frame(height: height)
+        }
+        .padding(.vertical)
+        .padding(.horizontal)
+        .background(.thickMaterial)
+    }
+    
+    func sendMessage() {
+        if let message = viewModel.sendMessage(text, in: chat) {
+            text = ""
+            messageIDToScroll = message.id
+        }
+    }
+    
+    let columns = [GridItem(.flexible(minimum: 10))]
+    
+    func getMessagesView(viewWidth: CGFloat) -> some View {
+        LazyVGrid(columns: columns, spacing: 0, pinnedViews: [.sectionHeaders]) {
+            let sectionMessages = viewModel.getSectionMessages(for: chat)
+            
+            ForEach(sectionMessages.indices, id: \.self) { sectionIndex in
+                let messages = sectionMessages[sectionIndex]
+                Section(header: sectionHeader(firstMessage: messages.first!)) {
+                    ForEach(messages) { message in
+                        let isReceived = message.type == .Received
+                        HStack {
+                            ZStack {
+                                Text(message.text)
+                                    .foregroundColor(Color.white)
+                                    .padding(.horizontal)
+                                    .padding(.vertical, 12)
+                                    .background(isReceived ? Color.black.opacity(0.5) : .green.opacity(0.8))
+                                    .cornerRadius(13)
+                                
+                            }
+                            .frame(width: viewWidth * 0.7, alignment: isReceived ? .leading : .trailing)
+                            .padding(.vertical)
+                            
+                        }
+                        .frame(maxWidth: .infinity, alignment: isReceived ? .leading : .trailing)
+                        .id(message.id)
+                    }
+                }
+            }
+        }
+    }
+    
+    func sectionHeader(firstMessage message: Message) -> some View {
+        ZStack {
+            Text(message.date.descriptiveString(dateStyle: .medium))
+                .foregroundColor(.white)
+                .font(.system(size: 14, weight: .regular))
+                .frame(width: 120)
+                .padding(.vertical, 5)
+                .background(Capsule().foregroundColor(.blue))
+        }
+        .padding(.vertical, 5)
+        .frame(maxWidth: .infinity)
+    }
+    
 }
 
 struct ChatInView_Previews: PreviewProvider {
     static var previews: some View {
-        ChatInView(chat: Chat.sampleChat[0])
-            .environmentObject(ChatViewModel())
+        NavigationView {
+            ChatInView(chat: Chat.sampleChat[0])
+                .environmentObject(ChatViewModel())
+            
+        }
     }
 }
--- Instagram Clone/View/ChatRowView.swift
+++ Instagram Clone/View/ChatRowView.swift
@@ -12,7 +12,7 @@ struct ChatRowView: View {
     let chat: Chat
     
     var body: some View {
-        HStack {
+        HStack(spacing: 20) {
             Image(chat.person.imgString)
                 .resizable()
                 .frame(width: 70, height: 70)
@@ -27,10 +27,26 @@ struct ChatRowView: View {
                         
                         Spacer()
                         
+                        Text(chat.messages.last?.date.descriptiveString() ?? "")
+                            .foregroundColor(Color.black)
+                    }
+                    HStack {
+                        Text(chat.messages.last?.text ?? "")
+                            .foregroundColor(.gray)
+                            .lineLimit(2)
+                            .frame(height: 50, alignment: .top)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.trailing, 40)
+                        
                     }
                 }
+                Circle()
+                    .foregroundColor(chat.hasUnreadMessage ? .blue : .clear)
+                    .frame(width: 18, height: 18)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
             }
         }
+        .frame(height: 80)
     }
 }
 
