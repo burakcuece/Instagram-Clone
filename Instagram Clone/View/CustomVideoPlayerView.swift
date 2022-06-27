819d1fc717fbe970a6a6b42b21fee62c0d3489eb
Burak Cüce <b.cuece@web.de>
2022-06-28 00:39:26 +0200

    - add CustomVideoPlayerView

---
+++ Instagram Clone/View/CustomVideoPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomVideoPlayerView.swift
+//  Instagram Clone
+//
+//  Created by Burak Cüce on 28.06.22.
+//
+
+import SwiftUI
+
+struct CustomVideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomVideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomVideoPlayerView()
+    }
+}
